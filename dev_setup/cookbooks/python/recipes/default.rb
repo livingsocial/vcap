541f09e81fdf091a02a555446151573e2089653b
Ben Bleything <ben.bleything@livingsocial.com>
2012-03-23 22:42:04 -0700

    add centos support to python cookbook

--- dev_setup/cookbooks/python/recipes/default.rb
+++ dev_setup/cookbooks/python/recipes/default.rb
@@ -6,8 +6,15 @@
 #
 #
 
-%w[ python-dev python-setuptools ].each do |pkg|
-  package pkg
+case node['platform']
+when 'ubuntu'
+  %w[ python-dev python-setuptools ].each do |pkg|
+    package pkg
+  end
+when 'centos'
+  %w[ python python-setuptools ].each do |pkg|
+    package pkg
+  end
 end
 
 bash "Installing pip" do
